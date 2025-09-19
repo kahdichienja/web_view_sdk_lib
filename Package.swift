d9666b75bcfe73fcf822f0b31f7d093c051073a6
kahdichienja <agooclinton@gmail.com>
2025-09-19 07:16:02 +0300

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "web_view_sdk_lib",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "web_view_sdk_lib",
+            targets: ["web_view_sdk_lib"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "web_view_sdk_lib"
+        ),
+        .testTarget(
+            name: "web_view_sdk_libTests",
+            dependencies: ["web_view_sdk_lib"]
+        ),
+    ]
+)
